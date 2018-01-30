ae0eca13540d6351e0796c5bf2f1253299b3baa1
Ilya Alonov <i.alonov@innopolis.ru>
2018-01-30 16:25:48 +0300

    Configured admin page

--- inno_lib/admin.py
+++ inno_lib/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Document, Genre, Tag, Author, DocumentInstance
 
-# Register your models here.
+admin.site.register(Document)
+admin.site.register(Genre)
+admin.site.register(Tag)
+admin.site.register(Author)
+admin.site.register(DocumentInstance)
