3a19c9372796a19e8c944d2b3b453c2fef014cf4
Ilya Alonov <i.alonov@innopolis.ru>
2018-01-30 16:25:17 +0300

    Added some models

--- inno_lib/models.py
+++ inno_lib/models.py
@@ -1,15 +1,83 @@
 from django.db import models
+import uuid
 
 
 class Document(models.Model):
-    title = models.CharField(max_length=200)
-    # author = models.ManyToManyField(Author, help_text='select an author for this document')
-    price = models.IntegerField(max_length=10)
+    """
+    Model representing any Document in a system (but not a specific copy of one).
+    """
+    title = models.CharField(max_length=100)
+    price = models.IntegerField
+    authors = models.ManyToManyField(
+        'Author',
+        help_text='Add authors for this book',
+    )
+    tags = models.ManyToManyField(
+        'Tag',
+        help_text='Add tags for this book',
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class DocumentInstance(models.Model):
+    """
+    Model representing a specific copy of a Document
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique id for this particular document")
+    document = models.ForeignKey('Document', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS,
+                              blank=True, default='m', help_text='Document availability')
+
+    class Meta:
+        ordering = ["due_back"]
+
+    def __str__(self):
+        return '{0} ({1})'.format(self.id, self.document.title)
+
+
+class Genre(models.Model):
+    """
+    Model representing a Genre.
+    """
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=50)
-    # document = models.ManyToManyField(Document, help_text='select a document written by this author')
+    """
+    Model representing an Author
+    """
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+
+    def __str__(self):
+        return '{0}, {1}'.format(self.first_name, self.last_name)
+
+
+class Tag(models.Model):
+    """
+    Model representing a Tag (e.g. 'funny', 'scary' etc.)
+    """
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
--- inno_lib/views.py
+++ inno_lib/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Document, Author, Genre, Tag, DocumentInstance
 
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    """
+    View function for home page of site.
+    """
+    num_docs = Document.objects.all().count()
+    num_instances = DocumentInstance.objects.all().count()
+    num_instances_available = DocumentInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_docs': num_docs, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors},
+    )
