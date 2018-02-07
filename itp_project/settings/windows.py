8f96f7e0f9b6744eae59e9d70951154f14ec6c06
Gewery <35733988+Gewery@users.noreply.github.com>
2018-02-07 18:00:05 +0300

    Update windows.py

--- itp_project/settings/windows.py
+++ itp_project/settings/windows.py
@@ -1,9 +1,10 @@
-from . import *
-import os
-
-DATABASES = {
-    'default': {
+from . import * 
+import os 
+ 
+DATABASES = { 
+    'default': { 
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+    } 
+} 
+ 
